28b3d49a82ca2877d8bca8bfc3468acbe3ebeeaa
TimDunphy <timothy.dunphy@gmail.com>
2020-04-23 18:22:20 +0100

    Added error file location to messages.

--- helpers/complete.js
+++ helpers/complete.js
@@ -1,4 +1,5 @@
 const boxen = require('boxen')
+const path = require('path')
 
 const complete = db => {
     const sql = 'SELECT count(*) FROM releases;'
@@ -6,6 +7,14 @@ const complete = db => {
     db.all(sql, (err, rows) => {
         console.log(boxen(`Parsing complete - ${rows[0]['count(*)']} releases added to the database.`, {
             borderColor: 'green',
+            borderStyle: 'round',
+            dimBorder: true,
+            padding: 1,
+            margin: 1
+        }))
+
+        console.log(boxen(`Errors can be found at - ${path.join(__dirname, '/../errorCatcher.xml')}`, {
+            borderColor: 'red',
             borderStyle: 'round',
             dimBorder: true,
             padding: 1,
